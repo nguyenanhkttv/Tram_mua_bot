35b715a377f5e621734ac4712912be46abd0dc7d
nguyenanhkttv <nguyenanh.kttv@gmail.com>
2026-08-10 22:48:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ from playwright.async_api import async_playwright
 
 # 1. KHAI BÁO THƯ VIỆN GEMINI AI (Thêm 2 dòng này để hết gạch đỏ)
 import google.generativeai as genai
-from google.genai import types
 
 # 2. TỰ ĐỘNG TẢI CHROMIUM CHO PLAYWRIGHT
 try:
